133c7cc4443f4b09a91f620bf6ac075195220912
scott <scott.waye@hubse.com>
2018-12-13 13:01:45 -0500

    init commit

---
+++ UwpRefProblem/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+namespace TestBuildRefs
+{
+
+    public class DataErrorI : IDataErrorInfo
+    {
+        public string Error { get; }
+
+        public string this[string columnName] => throw new NotImplementedException();
+    }
+}
